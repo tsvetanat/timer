b7234895dac1ca50056613de8199e0b5001bfa1d
tsvetanat <132619729+tsvetanat@users.noreply.github.com>
2023-08-09 16:24:46 -0700

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -6,77 +6,74 @@
              * Implement Tasks using ES6 syntax and arrow functions
 */
 
-var div = document.querySelector(".stopwatch-display");
+let div = document.querySelector(".stopwatch-display");
 
-        const firstButtonHandler = () => {            
-            div.classList.remove("red");
-            div.classList.remove("silver");
-            div.classList.add("green");
-        },
+const firstButtonHandler = () => {            
+    div.classList.remove("red");
+    div.classList.remove("silver");
+    div.classList.add("green");
+},
 
-            secondButtonHandler = () => {
-                div.classList.remove("green");
-                div.classList.remove("silver");
-                div.classList.add("red");
+secondButtonHandler = () => {
+    div.classList.remove("green");
+    div.classList.remove("silver");
+    div.classList.add("red");           
+},
+
+thirdButtonHandler = () => {    
+    div.classList.remove("red");
+    div.classList.remove("green");
+    div.classList.add("silver");         
+};
+
+let counter = 0, intervalHandler;
+
+const ev = document.querySelector(".stopwatch-control");
+const display = document.querySelector(".stopwatch-display");
+            
+const count = () => {
+    let h = parseInt(counter/216000);
+    let res = parseInt(counter-216000*h);
+    let m = parseInt(res/3600);
+    let s = parseInt((res - 3600*m)/90);
                 
-            },
+    function getTwoDigits(n){
+    return (n < 10 ? "0" : "") + n;
+    }
 
-            thirdButtonHandler = () => {    
-                div.classList.remove("red");
-                div.classList.remove("green");
-                div.classList.add("silver");
-              
-            };
+    display.lastElementChild.innerText = getTwoDigits(s); 
+    display.firstElementChild.nextElementSibling.innerText =  getTwoDigits(m);
+    display.firstElementChild.innerText =  getTwoDigits(h);
 
-            let counter = 0, intervalHandler;
+    counter++;
+}
 
-            const ev = document.querySelector(".stopwatch-control");
-            const display = document.querySelector(".stopwatch-display");
+const reset= () => {
+    display.lastElementChild.innerText = "00";
+    display.firstElementChild.nextElementSibling.innerText = "00";
+    display.firstElementChild.innerText = "00";
+    counter = 0;
+}
+
+let startTimer = () => {
+    intervalHandler = setInterval(count, 10);
+}
+
+ev.firstElementChild.onclick = () => {
+    intervalHandler = setInterval(count, 10);
+    firstButtonHandler(); 
+}
             
-            const count = () => {
-                let h = parseInt(counter/216000);
-                let res = parseInt(counter-216000*h);
-                let m = parseInt(res/3600);
-                let s = parseInt((res - 3600*m)/60);
-                
-                function makeTwoDigits(n){
-                    return (n < 10 ? "0" : "") + n;
-                }
+ev.firstElementChild.nextElementSibling.onclick = () => {
+    clearInterval(intervalHandler);
+    secondButtonHandler();
+}
 
-                display.lastElementChild.innerText = makeTwoDigits(s); 
-                display.firstElementChild.nextElementSibling.innerText =  makeTwoDigits(m);
-                display.firstElementChild.innerText =  makeTwoDigits(h);
-
-                counter++;
-            }
-
-            const reset= () => {
-                display.lastElementChild.innerText = "00";
-                display.firstElementChild.nextElementSibling.innerText = "00";
-                display.firstElementChild.innerText = "00";
-            }
-
-            let startTimer = () => {
-                intervalHandler = setInterval(count, 10);
-            }
-
-            ev.firstElementChild.onclick = () => {
-                intervalHandler = setInterval(count, 10);
-                firstButtonHandler(); 
-            }
-            
-
-            ev.firstElementChild.nextElementSibling.onclick = () => {
-                clearInterval(intervalHandler);
-                secondButtonHandler();
-            }
-
-            
-            ev.lastElementChild.onclick = () => { 
-                thirdButtonHandler();
-                clearInterval(intervalHandler);
-                reset();
-            } 
+ev.lastElementChild.onclick = () => { 
+    thirdButtonHandler();
+    clearInterval(intervalHandler);
+    reset();
+} 
 
     
 
